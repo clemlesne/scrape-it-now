8baaacb528f9f171507ef77cc9e079ace2ac8fa1
Clémence Lesné <clemence@lesne.pro>
2024-09-11 17:05:19 +0200

    ci: Fix unit test error code

--- cicd/test-unit-ci.sh
+++ cicd/test-unit-ci.sh
@@ -2,11 +2,16 @@
 
 # Start the first command in the background
 make test-static-server &
+
 # Capture the PID of the background process
 UNIT_RUN_PID=$!
 
 # Run the second command
 make test-unit-run
+exit_code=$?
 
 # Once the second command exits, kill the first process
 kill $UNIT_RUN_PID
+
+# Exit with the same exit code as the second command
+exit $exit_code

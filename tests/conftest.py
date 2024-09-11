2ca42b37365c43c4a3a9e7da13ab343bedcc513c
Clémence Lesné <clemence@lesne.pro>
2024-09-11 18:01:01 +0200

    fix: Playwright browser lifecycle in unit test

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+import asyncio
+from collections.abc import AsyncGenerator
+
+import pytest
+from playwright.async_api import Browser, async_playwright
+
+from app.scrape import _get_broswer, _install_browser, _install_pandoc
+
+
+@pytest.fixture
+async def browser() -> AsyncGenerator[Browser, None]:
+    """
+    Fixture to provide a Playwright browser for each test.
+    """
+    async with async_playwright() as p:
+        browser_type = p.chromium
+
+        # Make sure the browser and pandoc are installed
+        await asyncio.gather(
+            # Install Playwright
+            _install_browser(browser_type),
+            # Install Pandoc
+            _install_pandoc(),
+        )
+
+    async with async_playwright() as p:
+        browser_type = p.chromium
+
+        async with await _get_broswer(browser_type) as browser:
+            yield browser

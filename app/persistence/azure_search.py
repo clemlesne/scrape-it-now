clemlesne/scrape-it-now

commit bbc49df21475bab828517bb4eddcf3ba1c70eeaf

security: Upgrade deps

app/persistence/azure_search.py
@@ -10,8 +10,8 @@ from azure.core.exceptions import (
 from azure.search.documents.aio import SearchClient
 from azure.search.documents.indexes.aio import SearchIndexClient
 from azure.search.documents.indexes.models import (
-    AzureOpenAIParameters,
     AzureOpenAIVectorizer,
+    AzureOpenAIVectorizerParameters,
     HnswAlgorithmConfiguration,
     LexicalAnalyzerName,
     SearchableField,
@@ -160,8 +160,8 @@ class AzureSearch(ISearch):
             ],
             vectorizers=[
                 AzureOpenAIVectorizer(
-                    name="vectorizer-default",
-                    azure_open_ai_parameters=AzureOpenAIParameters(
+                    vectorizer_name="vectorizer-default",
+                    azure_open_ai_parameters=AzureOpenAIVectorizerParameters(
                         api_key=self._config.azure_openai_api_key,
                         deployment_id=self._config.azure_openai_embedding_deployment,
                         model_name=self._config.azure_openai_embedding_model,

commit 317c1dc52d2f64d80ea76f8d7b65eb580a7946e9

fix: AI Search index creation
Bug introduced in: bbc49df21475bab828517bb4eddcf3ba1c70eeaf

app/persistence/azure_search.py
@@ -150,7 +150,7 @@ class AzureSearch(ISearch):
                 VectorSearchProfile(
                     algorithm_configuration_name="algorithm-default",
                     name="profile-default",
-                    vectorizer="vectorizer-default",
+                    vectorizer_name="vectorizer-default",
                 ),
             ],
             algorithms=[
@@ -161,11 +161,11 @@ class AzureSearch(ISearch):
             vectorizers=[
                 AzureOpenAIVectorizer(
                     vectorizer_name="vectorizer-default",
-                    azure_open_ai_parameters=AzureOpenAIVectorizerParameters(
+                    parameters=AzureOpenAIVectorizerParameters(
                         api_key=self._config.azure_openai_api_key,
-                        deployment_id=self._config.azure_openai_embedding_deployment,
+                        deployment_name=self._config.azure_openai_embedding_deployment,
                         model_name=self._config.azure_openai_embedding_model,
-                        resource_uri=self._config.azure_openai_endpoint,
+                        resource_url=self._config.azure_openai_endpoint,
                     ),
                 )
             ],
f87a3ea45cfc6822a6e5ea547e26f32796d608fa
Clémence Lesné <clemence@lesne.pro>
2024-11-09 13:48:39 +0100

    quality: Code lint

--- src/scrape_it_now/helpers/__init__.py
+++ src/scrape_it_now/helpers/__init__.py
@@ -1,6 +1,8 @@
 from dotenv import find_dotenv, load_dotenv
 
-load_dotenv(find_dotenv(
-  # Use the current working directory from where the command is run
-  usecwd=True,
-))
+load_dotenv(
+    find_dotenv(
+        # Use the current working directory from where the command is run
+        usecwd=True,
+    )
+)

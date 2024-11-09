ff68ca51a3ccf421b4a6dd2fae62a747cd00faa2
Clémence Lesné <clemence@lesne.pro>
2024-11-09 13:33:24 +0100

    fix: .env load from current path

--- src/scrape_it_now/helpers/__init__.py
+++ src/scrape_it_now/helpers/__init__.py
@@ -1,3 +1,6 @@
 from dotenv import find_dotenv, load_dotenv
 
-load_dotenv(find_dotenv())
+load_dotenv(find_dotenv(
+  # Use the current working directory from where the command is run
+  usecwd=True,
+))
